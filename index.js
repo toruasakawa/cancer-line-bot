77fb212172041cb1d2f3f37cea5bac19465b70b4
root <101169343+toruasakawa@users.noreply.github.com>
2026-03-27 14:30:21 +0900

    fix index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
+const express = require("express");
+const line = require("@line/bot-sdk");
 
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
 
 const client = new line.Client(config);
 const app = express();
 
-app.post('/webhook', line.middleware(config), (req, res) => {
+app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
-    .then(() => res.json({ status: 'ok' }))
-    .catch(err => res.status(500).end());
+    .then(() => res.json({ status: "ok" }))
+    .catch((err) => res.status(500).end());
 });
 
 async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') return;
+  if (event.type !== "message" || event.message.type !== "text") return;
   return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text // オウム返し
+    type: "text",
+    text: event.message.text, // オウム返し
   });
 }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Bot running on port ${port}`));
-```
-
-**.gitignore**
-```
-node_modules/
